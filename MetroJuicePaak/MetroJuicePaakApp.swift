02b25ab7d05ded410553f4e8ed232dd2690d16d4
Noah <noahangshihern@gmail.com>
2026-03-17 14:16:54 +0800

    Set up project files

---
+++ MetroJuicePaak/MetroJuicePaakApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroJuicePaakApp.swift
+//  MetroJuicePaak
+//
+//  Created by Noah Ang Shi Hern on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MetroJuicePaakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
