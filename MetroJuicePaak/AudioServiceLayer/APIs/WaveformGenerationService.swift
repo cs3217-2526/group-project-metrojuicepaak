707125828a06a30dba999182e6a112d0aca3fa32
Vince Leong <vinceleong04@gmail.com>
2026-04-03 23:33:42 +0800

    Change return type to WaveformData in generateWaveform
    Updated return type and documentation for generateWaveform function.

--- MetroJuicePaak/AudioServiceLayer/APIs/WaveformGenerationService.swift
+++ MetroJuicePaak/AudioServiceLayer/APIs/WaveformGenerationService.swift
@@ -12,9 +12,9 @@ protocol WaveformGenerationService {
     /// - Parameters:
     ///   - sample: The AudioSample to analyze
     ///   - resolution: Number of data points to generate (e.g., 100 for 100 bars)
-    /// - Returns: Array of normalized amplitude values (0.0 to 1.0)
+    /// - Returns: WaveformData represented by array of normalized amplitude values (0.0 to 1.0)
     func generateWaveform(
         for sample: AudioSample,
         resolution: Int
-    ) async -> [Float]
+    ) async -> WaveformData
 }
